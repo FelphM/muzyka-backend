0684902c3a9d04a8ddea707b2714d0ed08db79cc
EmersonPuebla <em.puebla@duocuc.cl>
2025-11-03 14:28:07 -0300

    🌟 Add TestControllerV1

---
+++ src/main/kotlin/com/alice/muzyka/controller/v1/TestControllerV1.kt
@@ -0,0 +1,13 @@
+package com.alice.muzyka.controller.v1
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController 
+class TestControllerV1 {
+
+    @GetMapping("/test")
+    fun getHelloWorld(): String {
+        return "Hello World"
+    }
+}
