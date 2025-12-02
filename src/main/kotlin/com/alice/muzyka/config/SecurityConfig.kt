aab0a15b63802c307b046e155f936a5dc670b65e
Felipe Moreira <felipe.moreira.fo@gmail.com>
2025-12-02 06:27:52 -0300

    Revert "update secConfig para blogs"
    This reverts commit 49d27370167eeae9dd31bf85bab759f7b5bbe723.

--- src/main/kotlin/com/alice/muzyka/config/SecurityConfig.kt
+++ src/main/kotlin/com/alice/muzyka/config/SecurityConfig.kt
@@ -91,7 +91,6 @@ class SecurityConfig(
                     .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/v1/blogposts/**").permitAll()
                     
                     // --- RUTAS PROTEGIDAS ---
                     .requestMatchers("/api/v1/orders/**").authenticated()
